d8a506e884096d6dc98666c8ebc1469caea5cbe2
DESKTOP-FPSVATK\zhouk <zhoukev@hotmail.com>
2020-09-19 19:21:56 -0400

    Added comments to Internal_grid.py

--- backend/Internal_Grid.py
+++ backend/Internal_Grid.py
@@ -1,15 +1,28 @@
-# TODO: Add comments
 class Grid:
+    # Create a grid object with the specified number of rows and columns
+    # INPUTS
+    # rows (int) - number of rows in the grid
+    # cols (int) - number of columns in the grid
     def __init__(self, rows, cols):
+        # Set the number of rows and columns in the grid
         self._rows = rows
         self._cols = cols
-        self._grid = []
+        # Set to store the coordinates (tuples) of all alive cells
         self._alive = set()
+        # Create the 2D array of cells
+        self._grid = []
         for i in range(rows):
             row = []
             for j in range(cols):
                 row.append(0)
             self._grid.append(row)
+
+    # Set the status of the cell at x and y, if the cell is alive (1), make it dead (0) and vice versa
+    # INPUTS
+    # x (int) - x-coordinate of the cell to update
+    # y (int) - y-coordinate of the cell to update
+    # OUTPUTS
+    # N/A - self_grid is updated in-place
 
     def set_status(self, x, y):
         if self._grid[x][y] == 1:
@@ -19,13 +32,28 @@ class Grid:
             self._grid[x][y] = 1
             self._alive.add((x, y))
 
+    # Return the status of the cell at x and y
+    # INPUTS
+    # x (int) - x-coordinate of the cell
+    # y (int) - y-coordinate of the cell
+    # OUTPUT
+    # self._grid[x][y] (int) the status of the cell (0 for dead), (1 for alive)
     def get_status(self, x, y):
         return self._grid[x][y]
 
+    # Update the cells in the grid by one generation, the rules are as follows:
+    # If a live cell has less than 2 live neighbours, it dies due to lack of resource
+    # If a live cell has more than 3 live neighbours, it dies due to overpopulation
+    # If a live cell has 2-3 live neighbours, it lives on to the next generation
+    # If a dead cell has exactly 3 live neighbours, it becomes live as through reproduction
+    # INPUTS
+    # N/A
+    # OUTPUTS
+    # N/A - self._grid is updated in-place
     def update(self):
         # neighbours: an array of tuples to assist in getting a cell's neighbours
         neighbours = [(1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1)]
-        # Add in all the cells we've checked into a set
+        # Create a set that keeps track of all the cells that we updated
         checked = set()
         # Apply the rules to each alive cell and their neighbours
         for i in self._alive:
@@ -44,10 +72,19 @@ class Grid:
                 self._grid[i[0]][i[1]] = 0
                 self._alive.remove((i[0], i[1]))
 
+    # Apply the game rules to the cell specified, used in conjunction with update()
+    # INPUTS
+    # board (int[][]) - grid that contains the status of each cell
+    # x (int) - x coordinate of the cell to update
+    # y (int) - y coordinate of the cell to update
+    # checked (set<(int,int)>) - set to store the coordinates (as a tuple) of all cells we have updated
+    # OUTPUTS
+    # N/A - board is updated in-place
     def _apply_rules(self, board, x, y, checked):
+        # neighbours: an array of tuples to assist in getting a cell's neighbours
         neighbours = [(1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1)]
+        # Count the number of live neighbours
         live_neighbours = 0
-        # Count the number of neighbours
         for neighbour in neighbours:
             r = x + neighbour[0]
             c = y + neighbour[1]
@@ -61,9 +98,14 @@ class Grid:
         # We use the 2 flag to represent that the cell was dead but is now live
         if board[x][y] == 0 and live_neighbours == 3:
             board[x][y] = 2
-        # Add the cell to the checked set so we can update it later
+        # Add the cell to the checked set so we can update it later in the update function
         checked.add((x, y))
-        return
+
+    # Clear the grid by setting all cells to be dead (0)
+    # INPUTS
+    # N/A
+    # OUTPUTS
+    # N/A - self._grid is updated inplace
 
     def clear(self):
         for i in range(self._rows):
