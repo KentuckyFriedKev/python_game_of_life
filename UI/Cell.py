0feb5e7551d3904ba10e6d97d74cb1390ebc9f2a
DESKTOP-FPSVATK\zhouk <zhoukev@hotmail.com>
2020-09-20 00:05:58 -0400

    Added comments to cell.py, fixed comment formatting in Internal_Grid.py

--- UI/Cell.py
+++ UI/Cell.py
@@ -2,33 +2,70 @@ import pygame
 
 # TODO: Add comments
 # TODO: optimize setting cells in UI (user should be able to hold mouse and set multiple cells
+"""
+ Represents a single cell on the game screen
+ Inherited from pygame sprite class
+"""
 
 
 class Cell(pygame.sprite.Sprite):
+    """
+    Creates a cell object
+
+    INPUTS:
+    x (int) - x-coordinate of the cell on the screen
+    y (int) - y-coordinate of the cell on the screen
+    i (int) - first index of the cell that corresponds to the internal grid
+    j (int) - second index of the cell that corresponds to the internal grid
+    grid (Grid) - Instance of a grid class which keeps track of the status of each cell
+    """
     def __init__(self, x,y, i, j, grid):
         super().__init__()
         self.i = i
         self.j = j
         self.grid = grid
+        # Create 5 by 5px images to represent the cells
+        # live cells are black
         self.live_cell = pygame.Surface((5, 5))
         self.live_cell.fill(pygame.Color(0,0,0))
+        # dead cells are white
         self.dead_cell = pygame.Surface((5, 5))
         self.dead_cell.fill(pygame.Color(255, 255, 255))
+        # cells will turn light gray when hovered upon
         self.hover_cell = pygame.Surface((5, 5))
         self.hover_cell.fill(pygame.Color(150, 150, 150))
+        # cells will turn dark gray when clicked on
         self.pressed_cell = pygame.Surface((5,5))
         self.pressed_cell.fill(pygame.Color(100,100,100))
         # Inherited from pygame sprite
+        # Set first image of cell to be a dead cell
         self.image = self.dead_cell
+        # Set collision boundaries for the cell
         self.rect = self.image.get_rect(topleft=(x, y))
-
+        # bool to see if the button has been pressed
         self.pressed = False
+
+    """
+    Update the image of the cell based off the cell's status from the internal grid (1 means alive, 0 means dead)
+    INPUTS
+    N/A
+    OUTPUTS
+    N/A
+    """
 
     def check_status(self):
         if self.grid.get_status(self.i, self.j) == 1:
             self.image = self.live_cell
         else:
             self.image = self.dead_cell
+
+    """
+    Event handler for the cell, handles MOUSEBUTTONDOWN, MOUSEBUTTONUP, and MOUSEMOTION events
+    INPUTS
+    event (pygame.event) - event in pygame
+    OUTPUTS
+    N/A
+    """
 
     def handle_event(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN:
--- backend/Internal_Grid.py
+++ backend/Internal_Grid.py
@@ -1,8 +1,13 @@
 class Grid:
-    # Create a grid object with the specified number of rows and columns
-    # INPUTS
-    # rows (int) - number of rows in the grid
-    # cols (int) - number of columns in the grid
+
+    """
+    Create a grid object with the specified number of rows and columns
+
+    INPUTS
+    rows (int) - number of rows in the grid
+    cols (int) - number of columns in the grid
+    """
+
     def __init__(self, rows, cols):
         # Set the number of rows and columns in the grid
         self._rows = rows
@@ -17,12 +22,15 @@ class Grid:
                 row.append(0)
             self._grid.append(row)
 
-    # Set the status of the cell at x and y, if the cell is alive (1), make it dead (0) and vice versa
-    # INPUTS
-    # x (int) - x-coordinate of the cell to update
-    # y (int) - y-coordinate of the cell to update
-    # OUTPUTS
-    # N/A - self_grid is updated in-place
+    """
+    Set the status of the cell at x and y, if the cell is alive (1), make it dead (0) and vice versa
+    
+    INPUTS
+    x (int) - x-coordinate of the cell to update
+    y (int) - y-coordinate of the cell to update
+    OUTPUTS
+    N/A - self_grid is updated in-place
+    """
 
     def set_status(self, x, y):
         if self._grid[x][y] == 1:
@@ -32,24 +40,30 @@ class Grid:
             self._grid[x][y] = 1
             self._alive.add((x, y))
 
-    # Return the status of the cell at x and y
-    # INPUTS
-    # x (int) - x-coordinate of the cell
-    # y (int) - y-coordinate of the cell
-    # OUTPUT
-    # self._grid[x][y] (int) the status of the cell (0 for dead), (1 for alive)
+    """
+    Return the status of the cell at x and y
+    INPUTS
+    x (int) - x-coordinate of the cell
+    y (int) - y-coordinate of the cell
+    OUTPUT
+    self._grid[x][y] (int) the status of the cell (0 for dead), (1 for alive)
+    """
+
     def get_status(self, x, y):
         return self._grid[x][y]
 
-    # Update the cells in the grid by one generation, the rules are as follows:
-    # If a live cell has less than 2 live neighbours, it dies due to lack of resource
-    # If a live cell has more than 3 live neighbours, it dies due to overpopulation
-    # If a live cell has 2-3 live neighbours, it lives on to the next generation
-    # If a dead cell has exactly 3 live neighbours, it becomes live as through reproduction
-    # INPUTS
-    # N/A
-    # OUTPUTS
-    # N/A - self._grid is updated in-place
+    """
+    Update the cells in the grid by one generation, the rules are as follows:
+    If a live cell has less than 2 live neighbours, it dies due to lack of resource
+    If a live cell has more than 3 live neighbours, it dies due to overpopulation
+    If a live cell has 2-3 live neighbours, it lives on to the next generation
+    If a dead cell has exactly 3 live neighbours, it becomes live as through reproduction
+    INPUTS
+    N/A
+    OUTPUTS
+    N/A - self._grid is updated in-place
+    """
+
     def update(self):
         # neighbours: an array of tuples to assist in getting a cell's neighbours
         neighbours = [(1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1)]
@@ -72,14 +86,17 @@ class Grid:
                 self._grid[i[0]][i[1]] = 0
                 self._alive.remove((i[0], i[1]))
 
-    # Apply the game rules to the cell specified, used in conjunction with update()
-    # INPUTS
-    # board (int[][]) - grid that contains the status of each cell
-    # x (int) - x coordinate of the cell to update
-    # y (int) - y coordinate of the cell to update
-    # checked (set<(int,int)>) - set to store the coordinates (as a tuple) of all cells we have updated
-    # OUTPUTS
-    # N/A - board is updated in-place
+    """
+    Apply the game rules to the cell specified, used in conjunction with update()
+    INPUTS
+    board (int[][]) - grid that contains the status of each cell
+    x (int) - x coordinate of the cell to update
+    y (int) - y coordinate of the cell to update
+    checked (set<(int,int)>) - set to store the coordinates (as a tuple) of all cells we have updated
+    OUTPUTS
+    N/A - board is updated in-place
+    """
+
     def _apply_rules(self, board, x, y, checked):
         # neighbours: an array of tuples to assist in getting a cell's neighbours
         neighbours = [(1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1)]
@@ -101,11 +118,13 @@ class Grid:
         # Add the cell to the checked set so we can update it later in the update function
         checked.add((x, y))
 
-    # Clear the grid by setting all cells to be dead (0)
-    # INPUTS
-    # N/A
-    # OUTPUTS
-    # N/A - self._grid is updated inplace
+    """
+    Clear the grid by setting all cells to be dead (0)
+    INPUTS
+    N/A
+    OUTPUTS
+    N/A - self._grid is updated inplace
+    """
 
     def clear(self):
         for i in range(self._rows):
